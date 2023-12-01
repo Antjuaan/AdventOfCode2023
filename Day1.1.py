ce508d7b07bb9533cab22b6c9a32b9e0487f3ab3
Antonio <105592572+Antjuaan@users.noreply.github.com>
2023-12-01 17:25:27 +0100

    Add files via upload

---
+++ Day1.1.py
@@ -0,0 +1,37 @@
+# Open the file
+file = open("Day1.txt", "r")
+
+# Save in a list each line of the file
+lines = file.readlines()
+all_numbers = []
+
+# For each line, save the numbers in a list
+for line in lines:
+    temp = []
+    for c in line:
+        if c.isdigit():
+            #Add the number to the list
+            temp.append(c)
+    all_numbers.append(temp)
+
+# For each list, if there is only one number, add another one of the same
+for list in all_numbers:
+    if len(list) == 1:
+        list.append(list[0])
+
+# Now put all the numbers in the list together
+final_list = []
+for list in all_numbers:
+    final_list.append(int(''.join(list)))
+
+# For every number in the list, keep only the first and the last digit, exept for the list where there are only two numbers
+for i in range(len(final_list)):
+    if len(all_numbers[i]) > 2:
+        final_list[i] = int(all_numbers[i][0] + all_numbers[i][-1])
+
+# Sum all the numbers
+sum = 0
+for number in final_list:
+    sum += number
+
+print(sum)
---
+++ Day1.2.py
@@ -0,0 +1,34 @@
+import re
+
+# Open the file
+file = open("Day1.txt", "r")
+file = file.read().splitlines()
+
+# Dictionary mapping words to their corresponding numeric values
+numbers = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+
+# Regular expression to match either a digit (\d) or any of the words in the 'numbers' dictionary
+regex = r'\d|' + r"|".join(numbers)
+
+# Regular expression to match either a digit (\d) or the reverse of any of the words in the 'numbers' dictionary
+regex_reverse = r'\d|' + r"|".join(d[::-1] for d in numbers)
+
+
+sum = 0
+for line in file:
+    # Find the first digit or word match in the 'line' based on the 'regex' pattern
+    first_digit = re.search(regex, line)[0]
+
+    # Reverse the 'line', find the last digit or word match based on the reversed 'regex_reverse' pattern, and then reverse the result to get the correct order
+    last_digit = re.search(regex_reverse, line[::-1])[0][::-1]
+
+    # Convert the words to their corresponding numeric values
+    for num in numbers:
+        if num == first_digit:
+            first_digit = numbers[num]
+        if num == last_digit:
+            last_digit = numbers[num]
+
+    # Now put together the two digits and sum them
+    sum += int(str(first_digit) + str(last_digit))
+print(sum)
